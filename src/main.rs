f621c54fc376df8fa04a496afd1f236f9a619a7a
Luna Ramirez <26529488+hyphena@users.noreply.github.com>
2024-03-22 14:55:11 -0400

    Standardize error output

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
 
     // identifies and stores command line arguments in a new Config object
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // using if let as there is no need to unwrap returned non-error values
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
